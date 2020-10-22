46b0db5d23ac4160f06f678b6c6610a821c04752
bettilaszlo <laszlobetti3@gmail.com>
2020-10-22 09:28:05 +0200

    Igeny és Auto osztályok létrehozása

---
+++ Telekocsi_Form/Auto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telekocsi_Form
+{
+    class Auto
+    {
+        public string Indulas { get; private set; }
+        public string Cel { get; private set; }
+        public string Rendszam { get; private set; }
+        public string Telefonszam { get; private set; }
+        public int Ferohely { get; private set; }
+        public string Utvonal { get; private set; }
+
+        public Auto(string indulas, string cel, string rendszam, string telefonszam, int ferohely)
+        {
+            Indulas = indulas;
+            Cel = cel;
+            Rendszam = rendszam;
+            Ferohely = Convert.ToInt32(ferohely);
+            Utvonal = Indulas + "-" + Cel;
+        }
+}
---
+++ Telekocsi_Form/Igeny.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telekocsi_Form
+{
+    class Igeny
+    {
+        public string Azonosito { get; private set; }
+        public string Indulas { get; private set; }
+        public string Cel { get; private set; }
+        public int Szemelyek { get; private set; }
+        public string Utvonal { get; private set; }
+
+        public Igeny(string azonosito, string indulas, string cel, int szemelyek)
+        {
+            Azonosito = azonosito;
+            Indulas = indulas;
+            Cel = cel;
+            Szemelyek = Convert.ToInt32(szemelyek);
+            Utvonal = Indulas + "-" + Cel;
+
+        }
+        public int VanAuto(List<Auto> auto)
+        {
+            int i = 0;
+            while (i < auto.Count &&
+                !(
+                Utvonal == auto[i].Utvonal &&
+                Szemelyek <= auto[i].Ferohely))
+            {
+                i++;
+            }
+            if (i < auto.Count)
+            {
+                return i;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
